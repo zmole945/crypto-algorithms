4b1f671404b82e48b9f9231f82c127c22b2c3743
shiz <zmole945@163.com>
2017-09-26 18:09:11 +0800

    add doxygen commet in des.h

--- des.h
+++ des.h
@@ -17,11 +17,11 @@
 #include <stdint.h>
 
 /****************************** MACROS ******************************/
-#define DES_BLOCK_SIZE 8                // DES operates on 8 bytes at a time
+#define DES_BLOCK_SIZE 8    /// DES operates on 8 bytes at a time
 
 /**************************** DATA TYPES ****************************/
-typedef unsigned char BYTE;             // 8-bit byte
-typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+typedef unsigned char BYTE; /// 8-bit byte
+typedef unsigned int  WORD; /// 32-bit word, change to "long" for 16-bit machines
 
 typedef enum {
     DES_ENCRYPT,
@@ -34,20 +34,48 @@ typedef enum {
 } des_mode_t;
 
 /*********************** FUNCTION DECLARATIONS **********************/
+
+//=================================================
+/// -
+/// des func
+/// @param in     input data
+/// @param out    output data
+/// @param key    des key
+/// @param mode   encrypt/decrypt
+/// @return       des result good/bad
+/// -
 int des_alg(    const uint8_t   *in,
                 uint8_t         *out,
                 const uint8_t   *key,
                 des_mode_t      mode);
 
-void des_key_setup(const BYTE key[], BYTE schedule[][6], DES_MODE mode);
-void des_crypt(const BYTE in[], BYTE out[], BYTE key[][6]);
+void des_key_setup( const BYTE  key[],
+                    BYTE        schedule[][6],
+                    DES_MODE    mode);
+void des_crypt( const BYTE  in[],
+                BYTE        out[],
+                BYTE        key[][6]);
 
-int tdes_alg(  const uint8_t   *in,
-                    uint8_t         *out,
-                    const uint8_t   *key,
-                    des_mode_t      mode);
+//=================================================
+/// -
+/// 3des func
+/// @param in     input data
+/// @param out    output data
+/// @param key    des key
+/// @param mode   encrypt/decrypt
+/// @return       des result good/bad
+/// -
+int tdes_alg(   const uint8_t   *in,
+                uint8_t         *out,
+                const uint8_t   *key,
+                des_mode_t      mode);
 
-void three_des_key_setup(const BYTE key[], BYTE schedule[][16][6], DES_MODE mode);
-void three_des_crypt(const BYTE in[], BYTE out[], BYTE key[][16][6]);
+void three_des_key_setup(   const BYTE  key[],
+                            BYTE        schedule[][16][6],
+                            DES_MODE    mode);
+void three_des_crypt(   const BYTE  in[],
+                        BYTE        out[],
+                        BYTE        key[][16][6]);
 
 #endif   // DES_H
+
