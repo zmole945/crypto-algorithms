147442ce3c323645e43e03a51d22dc3f6257c1b3
shiz <zmole945@163.com>
2017-10-12 11:29:57 +0800

    clean code

--- sha1.c
+++ sha1.c
@@ -1,13 +1,13 @@
-/*********************************************************************
- * Filename:   sha1.c
- * Author:     Brad Conte (brad AT bradconte.com)
- * Copyright:
- * Disclaimer: This code is presented "as is" without any guarantees.
- * Details:    Implementation of the SHA1 hashing algorithm.
- Algorithm specification can be found here:
- * http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
- This implementation uses little endian byte order.
- *********************************************************************/
+/**
+ * \file   sha1.c
+ * \brief
+ *       SHA1摘要算法的C语言实现 \n
+ *       算法使用小端序，移植到大端序平台時需進行相應轉換 \n
+ *       算法標準文檔[fips180-2]\n
+ *
+ *       當前维护者：Shiz(zmole945@163.com) \n
+ *       感谢創建最初源碼的Brad Conte (brad AT bradconte.com)
+ */
 
 /*************************** HEADER FILES ***************************/
 #include <stdlib.h>
@@ -18,14 +18,19 @@
 #define ROTLEFT(a, b) ((a << b) | (a >> (32 - b)))
 
 /*********************** FUNCTION DEFINITIONS ***********************/
-void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
+static void sha1_transform( sha1_ctx_t  *ctx,
+                            const BYTE  data[])
 {
     WORD a, b, c, d, e, i, j, t, m[80];
 
     for (i = 0, j = 0; i < 16; ++i, j += 4)
-        m[i] = (data[j] << 24) + (data[j + 1] << 16) + (data[j + 2] << 8) + (data[j + 3]);
+        m[i] =  (data[j  ] << 24) +
+                (data[j+1] << 16) +
+                (data[j+2] << 8 ) +
+                (data[j+3]      ) ;
+
     for ( ; i < 80; ++i) {
-        m[i] = (m[i - 3] ^ m[i - 8] ^ m[i - 14] ^ m[i - 16]);
+        m[i] = (m[i-3] ^ m[i-8] ^ m[i-14] ^ m[i-16]);
         m[i] = (m[i] << 1) | (m[i] >> 31);
     }
 
@@ -36,7 +41,9 @@ void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
     e = ctx->state[4];
 
     for (i = 0; i < 20; ++i) {
-        t = ROTLEFT(a, 5) + ((b & c) ^ (~b & d)) + e + ctx->k[0] + m[i];
+        t = ROTLEFT(a, 5) +
+            ((b & c) ^ (~b & d)) +
+            e + ctx->k[0] + m[i];
         e = d;
         d = c;
         c = ROTLEFT(b, 30);
@@ -44,7 +51,9 @@ void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
         a = t;
     }
     for ( ; i < 40; ++i) {
-        t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[1] + m[i];
+        t = ROTLEFT(a, 5) +
+            (b ^ c ^ d) +
+            e + ctx->k[1] + m[i];
         e = d;
         d = c;
         c = ROTLEFT(b, 30);
@@ -52,7 +61,9 @@ void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
         a = t;
     }
     for ( ; i < 60; ++i) {
-        t = ROTLEFT(a, 5) + ((b & c) ^ (b & d) ^ (c & d))  + e + ctx->k[2] + m[i];
+        t = ROTLEFT(a, 5) +
+            ((b & c) ^ (b & d) ^ (c & d)) +
+            e + ctx->k[2] + m[i];
         e = d;
         d = c;
         c = ROTLEFT(b, 30);
@@ -60,7 +71,9 @@ void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
         a = t;
     }
     for ( ; i < 80; ++i) {
-        t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[3] + m[i];
+        t = ROTLEFT(a, 5) +
+            (b ^ c ^ d) +
+            e + ctx->k[3] + m[i];
         e = d;
         d = c;
         c = ROTLEFT(b, 30);
@@ -78,21 +91,25 @@ void sha1_transform(sha1_ctx_t *ctx, const BYTE data[])
 int sha1_init(sha1_ctx_t *ctx)
 {
     ctx->datalen = 0;
-    ctx->bitlen = 0;
-    ctx->state[0] = 0x67452301;
-    ctx->state[1] = 0xEFCDAB89;
-    ctx->state[2] = 0x98BADCFE;
-    ctx->state[3] = 0x10325476;
-    ctx->state[4] = 0xc3d2e1f0;
-    ctx->k[0] = 0x5a827999;
-    ctx->k[1] = 0x6ed9eba1;
-    ctx->k[2] = 0x8f1bbcdc;
-    ctx->k[3] = 0xca62c1d6;
+    ctx->bitlen  = 0;
+
+    ctx->state[0]   = 0x67452301;
+    ctx->state[1]   = 0xEFCDAB89;
+    ctx->state[2]   = 0x98BADCFE;
+    ctx->state[3]   = 0x10325476;
+    ctx->state[4]   = 0xc3d2e1f0;
+
+    ctx->k[0]       = 0x5a827999;
+    ctx->k[1]       = 0x6ed9eba1;
+    ctx->k[2]       = 0x8f1bbcdc;
+    ctx->k[3]       = 0xca62c1d6;
 
     return 0;
 }
 
-int sha1_update(sha1_ctx_t *ctx, const BYTE data[], size_t len)
+int sha1_update(sha1_ctx_t  *ctx,
+                const BYTE  data[],
+                size_t      len)
 {
     size_t i;
 
@@ -129,24 +146,28 @@ int sha1_final(sha1_ctx_t *ctx, BYTE hash[])
         memset(ctx->data, 0, 56);
     }
 
-    // Append to the padding the total message's length in bits and transform.
+    // Append to the padding the total message's length in bits
+    // and transform.
     ctx->bitlen += ctx->datalen * 8;
-    ctx->data[63] = ctx->bitlen;
-    ctx->data[62] = ctx->bitlen >> 8;
+
+    ctx->data[63] = ctx->bitlen      ;
+    ctx->data[62] = ctx->bitlen >> 8 ;
     ctx->data[61] = ctx->bitlen >> 16;
     ctx->data[60] = ctx->bitlen >> 24;
     ctx->data[59] = ctx->bitlen >> 32;
     ctx->data[58] = ctx->bitlen >> 40;
     ctx->data[57] = ctx->bitlen >> 48;
     ctx->data[56] = ctx->bitlen >> 56;
+
     sha1_transform(ctx, ctx->data);
 
-    // Since this implementation uses little endian byte ordering and MD uses big endian,
-    // reverse all the bytes when copying the final state to the output hash.
+    // Since this implementation uses little endian byte ordering
+    // and MD uses big endian, reverse all the bytes when copying
+    // the final state to the output hash.
     for (i = 0; i < 4; ++i) {
-        hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+        hash[i     ] = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 4 ] = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+        hash[i + 8 ] = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
         hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
         hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
     }
